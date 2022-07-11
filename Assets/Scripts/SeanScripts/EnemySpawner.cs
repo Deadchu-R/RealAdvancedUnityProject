f2990a4f97a589488a4e9da82e23bdfb8056b0a1
Deadchu-R <93131102+Deadchu-R@users.noreply.github.com>
2022-07-11 23:35:29 +0300

    changes for spawner v0.5

--- Assets/Scripts/SeanScripts/EnemySpawner.cs
+++ Assets/Scripts/SeanScripts/EnemySpawner.cs
@@ -1,58 +1,91 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    [SerializeField] private GameObject skelaton;
+    [SerializeField] private GameObject skeleton;
     [SerializeField] private int surviveTimer;
     [SerializeField] private int amountOfEnemies;
+    private int _stage = 1;
     private bool isCreated = false;
-    private void OnTriggerEnter2D(Collider2D coll)
+    private bool _spawnerOn = false;
+
+    private void Start()
     {
-        if (coll.CompareTag("Player"))
+        InvokeRepeating(nameof(SpawnEnemy),1,2);
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
         {
+            _spawnerOn = true;
+            Debug.Log("spawner is on");
             //SpawnEnemy();
-            InvokeRepeating(nameof(UpdateTimer), 1, 1);
+            // InvokeRepeating(nameof(UpdateTimer), 1, 1);
 
             if (surviveTimer > 0)
             {
 
-                //Invoke(nameof(SpawnEnemy),0f);
+                // InvokeRepeating(nameof(SpawnEnemy),1,1);
                 //SpawnEnemy();
                 //if ()
                 //{
 
                 //}
             }
-            
-           
-            
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            _spawnerOn = false;
         }
     }
 
     private void Update()
     {
-        if (!isCreated)
-        {
-
-            GameObject skelatonClone = Instantiate(skelaton, transform.position, Quaternion.identity);
-            isCreated = true;
-            if (skelatonClone)
-            {
-
-            }
-        }
+        // if (!isCreated)
+        // {
+        //
+        //     GameObject skeletonClone = Instantiate(skeleton, transform.position, Quaternion.identity);
+        //     isCreated = true;
+        //     if (skeletonClone)
+        //     {
+        //
+        //     }
+        // }
         
     }
 
     void SpawnEnemy()
     {
-      
+        if (_spawnerOn)
+        {
+            if (_stage == 1)
+            {
+             var skeletonClone = Instantiate(skeleton, transform.position, Quaternion.identity);
+
+                skeletonClone.transform.parent = this.gameObject.transform;
+                // Enemy[] enemies = GetComponentInChildren<enemyBehaviour>();
+                // enemies = GetComponentsInChildren<Enemy>();
+                // amountOfEnemies = enemies.Length;
+            }
+
+        }
+
     }
     void UpdateTimer()
     {
         surviveTimer--;
                 Debug.Log(surviveTimer);
     }
+}
+
+internal class Enemy
+{
 }
